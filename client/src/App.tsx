e16b59b543cc2588b0f9d6be8fb33e9e5b466522
Tal Weiss <Tweiss747@gmail.com>
2024-02-21 15:16:52 +0200

    Created store
    Added Dimension store, Configured reducers, stores, and custom hooks for typescript features.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
-function App() {
+import { useAppDispatch, useAppSelector } from "./store/storeHooks"
+import { changeWidth, changeHeight } from "./store/dimensionSlice"
 
-  return (
+function App() {
+    const dispatch = useAppDispatch()
+    const height:number = useAppSelector(state => state.dimensions.height)
+    const width: number = useAppSelector(state => state.dimensions.width)
+    window.addEventListener('resize', () => {
+        dispatch(changeHeight())
+    })
+
+    window.addEventListener('resize', () => {
+        dispatch(changeWidth())
+    })
+
+
+    return (
     <>
             <h1>My Dashboard</h1>
+            <p>Current Height: {height}</p>
+            <p>Current Width: {width} </p>
     </>
   )
 }
