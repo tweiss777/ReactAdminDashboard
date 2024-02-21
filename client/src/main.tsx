cc74134981c3de20e7161f4dec4a89bb5d6304a0
Tal Weiss <tweiss747@gmail.com>
2024-02-21 15:22:38 +0200

    Formatting

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,7 @@ import { store } from './store/store.ts'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
         <Provider store={store} >
-            <App />
-            </Provider>
+          <App />
+        </Provider>
   </React.StrictMode>,
 )
