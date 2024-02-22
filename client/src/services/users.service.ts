214d1102405ac34399b9cbc3878c0d7469bf8273
Tal Weiss <Tweiss747@gmail.com>
2024-02-22 14:38:29 +0200

    Added user services with mock data.

---
+++ client/src/services/users.service.ts
@@ -0,0 +1,44 @@
+import { User } from "../types/Users";
+
+const mockData = [
+    {
+        id: 1,
+        firstName: "Mathias",
+        lastName: "Ivashnikov",
+        email: "mivashnikov0@ovh.net",
+        ipAddress: "105.204.140.32",
+        address: "4030 Hudson Alley",
+    },
+    {
+        id: 2,
+        firstName: "Mathias",
+        lastName: "Ivashnikov",
+        email: "mivashnikov0@ovh.net",
+        ipAddress: "105.204.140.32",
+        address: "4030 Hudson Alley",
+    },
+    {
+        id: 3,
+        firstName: "Mathias",
+        lastName: "Ivashnikov",
+        email: "mivashnikov0@ovh.net",
+        ipAddress: "105.204.140.32",
+        address: "4030 Hudson Alley",
+    },
+];
+export async function getUsers(): Promise<User[]> {
+    return new Promise<User[]>((resolve, _reject) => {
+        resolve(mockData);
+    });
+}
+
+export async function getUser(name: string): Promise<User[]> {
+    return new Promise<User[]>((resolve, _reject) => {
+        const filteredUsers: User[] = mockData.filter(user => name === user.firstName)
+        resolve(filteredUsers)
+    })
+
+
+    
+    
+}
