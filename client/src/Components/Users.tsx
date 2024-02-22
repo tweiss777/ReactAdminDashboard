f6ddd5f698a5c50aa9db5391c977446ea5d00b80
Tal Weiss <Tweiss747@gmail.com>
2024-02-22 19:02:40 +0200

    Implemented Table

--- client/src/Components/Users.tsx
+++ client/src/Components/Users.tsx
@@ -1,5 +1,92 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getUsers } from "../services/users.service";
+import { User } from "../types/Users";
+import { Table, Alert } from "antd";
+import type { TableProps } from "antd";
+import "../scss/Users.scss";
 
 export default function Users() {
-    return <h1>Users</h1>;
+    const [users, setUsers] = useState<User[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [hasError, setHasError] = useState<boolean>(false);
+
+    const columns: TableProps<User>["columns"] = [
+        {
+            title: "id",
+            dataIndex: "id",
+            key: "id",
+        },
+
+        {
+            title: "First Name",
+            dataIndex: "firstName",
+            key: "firstName",
+        },
+        {
+            title: "Last Name",
+            dataIndex: "lastName",
+            key: "lastName",
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+            key: "email",
+        },
+
+        {
+            title: "Address",
+            dataIndex: "address",
+            key: "address",
+        },
+
+        {
+            title: "IP Address",
+            dataIndex: "ipAddress",
+            key: "ipAddress",
+        },
+    ];
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const users: User[] = await getUsers();
+                setUsers(users);
+            } catch (error) {
+                setHasError(true);
+            } finally {
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 5000);
+            }
+        };
+        fetchUsers();
+    }, []);
+
+    const onRow = (_record: any, _rowIndex: any) => ({
+        onClick: (event: any) => {
+            console.log(event);
+        },
+    });
+
+    return (
+        <>
+            {hasError ? (
+                <Alert type={"error"} message={"Error Loading Data"} />
+            ) : (
+                <div className="users-table">
+                    <h1>Users</h1>
+                    <Table
+                        onRow={onRow}
+                        scroll={{ x: true }}
+                        size={"large"}
+                        pagination={{ pageSize: 10, position: ["bottomLeft"] }}
+                        columns={columns}
+                        loading={isLoading}
+                        dataSource={users}
+                        bordered={true}
+                    />
+                </div>
+            )}
+        </>
+    );
 }
--- client/src/services/users.service.ts
+++ client/src/services/users.service.ts
@@ -25,6 +25,22 @@ const mockData = [
         ipAddress: "105.204.140.32",
         address: "4030 Hudson Alley",
     },
+    {
+        id: 4,
+        firstName: "Mathias",
+        lastName: "Ivashnikov",
+        email: "mivashnikov0@ovh.net",
+        ipAddress: "105.204.140.32",
+        address: "4030 Hudson Alley",
+    },
+    {
+        id: 5,
+        firstName: "Mathias",
+        lastName: "Ivashnikov",
+        email: "mivashnikov0@ovh.net",
+        ipAddress: "105.204.140.32",
+        address: "4030 Hudson Alley",
+    }
 ];
 export async function getUsers(): Promise<User[]> {
     return new Promise<User[]>((resolve, _reject) => {
