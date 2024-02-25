7633dd046b605ce8a63aafed0354579551f90c87
Tal Weiss <Tweiss747@gmail.com>
2024-02-25 21:15:20 +0200

    Fixes to proxy settings

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        port: 3000,
+    server: { 
+        port :3000,
+        proxy:{
+            '/api': 'http://127.0.0.1:8080'
+        }
     },
     plugins: [react()],
 });
