40f7bb8ec99dfb58f988a320661b19d3d902cb00
Tal Weiss <Tweiss747@gmail.com>
2024-02-25 17:47:46 +0200

    Added custom error handler

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,26 @@
-import Fastify, { FastifyInstance } from "fastify";
+import Fastify, { FastifyError, FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import "dotenv/config";
 import cors from "@fastify/cors";
 import userRoutes from "./routes/users.route";
+import responseDTO from "./dtos/responseDTO";
 
 const { PORT: port } = process.env;
 const fastify: FastifyInstance = Fastify();
 
 fastify.register(cors);
 fastify.register(userRoutes, { prefix: '/v1/users' });
+
+
+fastify.setErrorHandler((error: FastifyError, req: FastifyRequest, reply: FastifyReply) => {
+   const response: responseDTO<{error: string }> = {
+        status: 500,
+        data: {
+            error: error.message
+        }
+    }
+    reply.status(500).send(response)
+
+})
 
 async function start() {
     try {
