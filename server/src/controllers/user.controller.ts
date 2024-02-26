f0d3085cff86eda6aeda0d23e959b3b7deb17746
Tal Weiss <Tweiss747@gmail.com>
2024-02-26 18:38:21 +0200

    Added pagination to getUsers
    refactored query to use pagination

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -3,34 +3,36 @@ import execute from "../services/sql.service";
 import User from "../types/User";
 import { FastifyRequest, FastifyReply } from "fastify";
 
-export async function getUsers(_req: FastifyRequest, reply: FastifyReply) {
-  try {
-    const query: string =
-      "SELECT id, first_name AS firstName, last_name AS lastName, email, ip_address AS ipAddress, address FROM Users";
-    const result: User = await execute<User>(query);
-
+// refactor query to handle pagination
+export async function getUsers(req: FastifyRequest, reply: FastifyReply) {
+    try {
+        let { page_number: pageNumber } = req.params as any;
+        pageNumber = pageNumber >= 1 ? parseInt(pageNumber) : 1;
+        const offset = ((pageNumber - 1) * 10).toString();
+        const query: string =
+            'SELECT id, first_name AS firstName, last_name AS lastName, email, ip_address AS ipAddress, address FROM Users LIMIT 10 OFFSET ?';
+        const result: User = await execute<User>(query, [offset]);
 
         const responseDTO: responseDTO<User> = {
             status: 200,
-            data: result
-        }
+            data: result,
+        };
 
-        return reply.status(200).send(responseDTO)
-
-  } catch (error) {
-        throw error
+        return reply.status(200).send(responseDTO);
+    } catch (error) {
+        throw error;
     }
 }
-export async function getUserCount(_req: FastifyRequest, reply: FastifyReply){
+export async function getUserCount(_req: FastifyRequest, reply: FastifyReply) {
     try {
-        const query: string = "SELECT COUNT(id) AS total_users FROM Users;"
-        const result: {count: number[]}[] = await execute(query)
-        const responseDto:responseDTO<{count: number[]}> = {
+        const query: string = "SELECT COUNT(id) AS total_users FROM Users;";
+        const result: { count: number[] }[] = await execute(query);
+        const responseDto: responseDTO<{ count: number[] }> = {
             status: 200,
-            data: result[0]
-        }
-        return reply.status(200).send(responseDto)
+            data: result[0],
+        };
+        return reply.status(200).send(responseDto);
     } catch (error) {
-        throw error
+        throw error;
     }
 }
