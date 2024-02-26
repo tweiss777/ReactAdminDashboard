bfcaf28614ef70fef2436403e136e9fce13efea5
Tal Weiss <Tweiss747@gmail.com>
2024-02-26 23:05:35 +0200

    Made Refactors to User Form

--- client/src/Components/User.tsx
+++ client/src/Components/User.tsx
@@ -3,18 +3,31 @@ import { Card, Button } from "antd";
 import "../scss/User.scss";
 
 interface IProps {
-    user: IUser;
+  user: IUser;
+    onClose: () => void
 }
 
-export default function User({ user }: IProps) {
+export default function User({ user, onClose }: IProps) {
+    function close(){
+        onClose()
+    }
     return (
-        <Card className="user-card" title={`${user.firstName} ${user.lastName}`} extra={<Button type="primary">Edit</Button>}>
-            <h2> Email</h2>
-            <h4> {user.email} </h4>
-            <h2> Address</h2>
-            <h4> {user.address}</h4>
-            <h2> IP Address </h2>
-            <h4>{user.ipAddress}</h4>
-        </Card>
-    );
+    <Card
+      className="user-card"
+      title={`${user.firstName} ${user.lastName}`}
+      extra={<Button type="primary">Edit</Button>}
+    >
+      <h2> Email</h2>
+      <h4> {user.email} </h4>
+      <h2> Address</h2>
+      <h4> {user.address}</h4>
+      <h2> IP Address </h2>
+      <h4>{user.ipAddress}</h4>
+      <div className="close-btn-container">
+        <Button onClick={close} className="close-btn" size="large">
+          Close
+        </Button>
+      </div>
+    </Card>
+  );
 }
