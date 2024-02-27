81cf14c32b12731d7dcb0fbf87936fd0c5cb8902
Tal Weiss <Tweiss747@gmail.com>
2024-02-27 17:59:21 +0200

    added update users

--- client/src/services/users.service.ts
+++ client/src/services/users.service.ts
@@ -1,19 +1,32 @@
 import { User } from "../types/Users";
-import axios from 'axios'
+import axios from "axios";
 export async function getUsers(pageNumber?: number): Promise<User[]> {
-   const { data: {data} }  = await axios.get(`api/v1/users/${pageNumber ?? 1}`)
-    return data as User[]
+    const {
+        data: { data },
+    } = await axios.get(`api/v1/users/${pageNumber ?? 1}`);
+    return data as User[];
 }
 
-export async function getUserCount(): Promise<number>{
-    const { data: {data} }  = await axios.get('api/v1/users/count')
-    return data.total_users
+export async function getUserCount(): Promise<number> {
+    const {
+        data: { data },
+    } = await axios.get("api/v1/users/count");
+    return data.total_users;
+}
+
+export async function updateUser(user: User) {
+    const {
+        data: { data },
+    } = await axios.put(`api/v1/users/${user.id}`, {
+        user,
+    });
+    return data.changedRows as number;
 }
 
 //export async function getUser(name: string): Promise<User[]> {
-  //  return new Promise<User[]>((resolve, _reject) => {
-    //    const filteredUsers: User[] = mockData.filter(user => name === user.firstName)
-      //  resolve(filteredUsers)
-   // })
-  
+//  return new Promise<User[]>((resolve, _reject) => {
+//    const filteredUsers: User[] = mockData.filter(user => name === user.firstName)
+//  resolve(filteredUsers)
+// })
+
 //}
