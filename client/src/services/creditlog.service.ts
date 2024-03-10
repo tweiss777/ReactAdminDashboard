822f62ec06ee38e49405654829c9afeb9869be23
Tal Weiss <Tweiss747@gmail.com>
2024-03-10 15:43:59 +0200

    call credit log count endpoint and pass total cound to pagination component

--- client/src/services/creditlog.service.ts
+++ client/src/services/creditlog.service.ts
@@ -1,6 +1,17 @@
 import { CreditLog } from "../types/CreditLog";
 import axios from "axios";
-export async function getCreditLogs(pageNumber: number = 1): Promise<CreditLog[]>{
-    const { data: {data}} = await axios.get(`/api/v1/credits?page_num=${pageNumber}`)
-    return data as CreditLog[]
+export async function getCreditLogs(
+  pageNumber: number = 1,
+): Promise<CreditLog[]> {
+  const {
+    data: { data },
+  } = await axios.get(`/api/v1/credits?page_num=${pageNumber}`);
+  return data as CreditLog[];
+}
+
+export async function getLogCount(): Promise<number> {
+  const {
+    data: { data },
+  } = await axios.get("/api/v1/credits/credit-log-count");
+  return data.count as number;
 }
