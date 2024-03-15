019447a628afe470ebe1efda8b04681b96bc699e
Tal Weiss <Tweiss747@gmail.com>
2024-03-15 19:52:40 +0200

    implemented login

--- client/src/Components/Login.tsx
+++ client/src/Components/Login.tsx
@@ -1,36 +1,63 @@
-import '../scss/Login.scss';
-import { Input, Button, Checkbox } from "antd";
-import { MailOutlined, KeyOutlined } from '@ant-design/icons';
-import { useState } from 'react'
-
+import "../scss/Login.scss";
+import { Input, Button, Checkbox, Alert } from "antd";
+import { MailOutlined, KeyOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { login } from "../services/authentication.service";
 
 export default function Login() {
+    const [loggingIn, setLoggingIn] = useState<boolean>(false);
+    const [error, setError] = useState<string>("");
 
-    const [loggingIn, setLoggingIn] = useState<boolean>(false)
-    
-    async function login(event:React.FormEvent<HTMLFormElement>){
-        event.preventDefault()
-        const formData: FormData = new FormData(event.currentTarget)
-        const email: string | undefined = formData.get('email')?.toString();
-        const password: string | undefined = formData.get('password')?.toString()
-        const rememberMe: string | undefined = formData.get('remember-me')?.toString()
+    async function loginUser(event: React.FormEvent<HTMLFormElement>) {
+        try {
+            if(error){
+                setError('')
+            }
+            setLoggingIn(true)
+            event.preventDefault();
+            const formData: FormData = new FormData(event.currentTarget);
+            const email: string | undefined = formData.get("email")?.toString();
+            const password: string | undefined = formData.get("password")?.toString();
+            const rememberMe: string | undefined = formData
+                .get("remember-me")
+                ?.toString();
+            if (password && email) {
+                const token = await login(email, password);
+                if(rememberMe){
+                    localStorage.setItem("token", token)
+                } else{
+                    sessionStorage.setItem("token", token)
+                }
+            } else {
+                setError("Email and password required");
+            }
+        } catch (error: any) {
+            setError(error.response.data.data.message) 
+        }
+        finally{
+            setLoggingIn(false)
+
+        }
     }
     return (
         <div className="login-container">
-            <form onSubmit={login} className='login-box'>
+            <form onSubmit={loginUser} className="login-box">
+                {error && <Alert  message={error} type={"error"} />}
                 <div>
                     <label>email</label>
-                    <Input name="email" prefix={<MailOutlined/> } />
+                    <Input name="email" prefix={<MailOutlined />} />
                 </div>
                 <div>
                     <label>password </label>
-                    <Input name='password' type="password" prefix={<KeyOutlined />} />
-                </div> 
-                <div>
-                    Remember Me <Checkbox name='remember-me' />
+                    <Input name="password" type="password" prefix={<KeyOutlined />} />
                 </div>
-                <div> 
-                    <Button loading={loggingIn} type="primary" htmlType='submit'>Login </Button>
+                <div>
+                    Remember Me <Checkbox name="remember-me" />
+                </div>
+                <div>
+                    <Button loading={loggingIn} type="primary" htmlType="submit">
+                        Login{" "}
+                    </Button>
                 </div>
             </form>
         </div>
