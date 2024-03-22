f3598202c780d34e932741cffa5c24407298b111
Tal Weiss <Tweiss747@gmail.com>
2024-03-22 14:26:14 +0200

    injected protected route and hide navbar if not authenticated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useAppDispatch } from "./store/storeHooks";
+import { useAppDispatch, useAppSelector } from "./store/storeHooks";
 import { changeWidth, changeHeight } from "./store/dimensionSlice";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NavBar } from "./Components/Navbar";
@@ -6,30 +6,53 @@ import Users from "./Components/Users";
 import Affiliates from "./Components/Affiliates";
 import CreditLog from "./Components/CreditLog";
 import Login from "./Components/Login";
+import ProtectedComponent from "./Components/ProtectedComponent";
 function App() {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
+  const isLoggedIn = useAppSelector((state) => state.authentication.isLoggedIn);
+  window.addEventListener("resize", () => {
+    dispatch(changeHeight());
+  });
 
-    window.addEventListener("resize", () => {
-        dispatch(changeHeight());
-    });
+  window.addEventListener("resize", () => {
+    dispatch(changeWidth());
+  });
 
-    window.addEventListener("resize", () => {
-        dispatch(changeWidth());
-    });
-
-    return (
-        <>
-            <BrowserRouter>
-                <NavBar />
-                <Routes>
-                    <Route path="/" element={<Users />} />
-                    <Route path="/affiliates" element={<Affiliates />} />
-                    <Route path="/credits" element={<CreditLog />} />
-                    <Route path="/login" element={<Login/>} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        {isLoggedIn && <NavBar />}
+        <Routes>
+          {/*todo encapsulate all routes but login with Protected route component*/}
+          <Route
+            path="/"
+            element={
+              <ProtectedComponent>
+                <Users />
+              </ProtectedComponent>
+            }
+          />
+          <Route
+            path="/affiliates"
+            element={
+              <ProtectedComponent>
+                <Affiliates />
+              </ProtectedComponent>
+            }
+          />
+          <Route
+            path="/credits"
+            element={
+              <ProtectedComponent>
+                <CreditLog />
+              </ProtectedComponent>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
