9b2869ea79a23566a4c6031220653ad509d033ca
Tal Weiss <Tweiss747@gmail.com>
2024-03-22 14:23:11 +0200

    pass jwt to authentication header

--- client/src/services/creditlog.service.ts
+++ client/src/services/creditlog.service.ts
@@ -1,12 +1,18 @@
 import { CreditLog } from "../types/CreditLog";
 import axios from "axios";
+import getToken from "../utils/getToken";
 export async function getCreditLogs(
   pageNumber: number = 1,
 ): Promise<CreditLog[]> {
   try {
+    const token = getToken();
     const {
       data: { data },
-    } = await axios.get(`/api/v1/credits?page_num=${pageNumber}`);
+    } = await axios.get(`/api/v1/credits?page_num=${pageNumber}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     return data as CreditLog[];
   } catch (error) {
     throw error;
@@ -15,9 +21,14 @@ export async function getCreditLogs(
 
 export async function getLogCount(): Promise<number> {
   try {
+    const token = getToken();
     const {
       data: { data },
-    } = await axios.get("/api/v1/credits/credit-log-count");
+    } = await axios.get("/api/v1/credits/credit-log-count", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     return data.count as number;
   } catch (error) {
     throw error;
--- client/src/services/users.service.ts
+++ client/src/services/users.service.ts
@@ -1,28 +1,57 @@
 import { User } from "../types/Users";
 import axios from "axios";
 import debounce from "../utils/debounce";
+import getToken from "../utils/getToken";
 
-export async function getUserCount(name: string=''): Promise<number> {
-    const {
-        data: { data },
-    } = await axios.get(`api/v1/users/count?name=${name}`);
-    return data.total_users;
+export async function getUserCount(name: string = ""): Promise<number> {
+  const token = getToken();
+  const {
+    data: { data },
+  } = await axios.get(`api/v1/users/count?name=${name}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data.total_users;
 }
 
 export async function updateUser(user: User) {
-    const {
-        data: { data },
-    } = await axios.put(`api/v1/users/${user.id}`, {
-        user,
-    });
-    return data.changedRows as number;
+  const token = getToken();
+  const {
+    data: { data },
+  } = await axios.put(
+    `api/v1/users/${user.id}`,
+    {
+      user,
+    },
+
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+  return data.changedRows as number;
 }
 
-export async function getUsers(pageNumber: number = 1,name: string=''): Promise<User[]> {
-    const fetchUsers = debounce(async (name: string, pageNumber: number) => {
-        const {data: { data }} = await axios.get(`/api/v1/users?first_name=${name}&page_number=${pageNumber}`)
-        return data
-    },1500)
-    const results = await fetchUsers(name, pageNumber) as User[]
-    return results
+export async function getUsers(
+  pageNumber: number = 1,
+  name: string = "",
+): Promise<User[]> {
+  const fetchUsers = debounce(async (name: string, pageNumber: number) => {
+    const token = getToken();
+    const {
+      data: { data },
+    } = await axios.get(
+      `/api/v1/users?first_name=${name}&page_number=${pageNumber}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    return data;
+  }, 1500);
+  const results = (await fetchUsers(name, pageNumber)) as User[];
+  return results;
 }
