e1770de610069ef3f2c6e43bcbe167426175f98a
Tal Weiss <Tweiss747@gmail.com>
2024-03-29 01:34:45 +0200

    injected schema for new user in route

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,13 +1,27 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
-import { authorize, login, registerUser, verifyToken } from "../controllers/auth.controller";
+import {
+    authorize,
+    login,
+    registerUser,
+    verifyToken,
+} from "../controllers/auth.controller";
+import newUserSchema from "../schemas/user-schema";
 
 export default async function authRoutes(
     fastify: FastifyInstance,
     options: FastifyPluginOptions,
-    ) { 
+) {
+    fastify.post(
+        "/register",
+        {
+            schema: {
+                body: newUserSchema,
+            },
+        },
 
-    fastify.post('/register', registerUser)
-    fastify.post('/verify', verifyToken)
-    fastify.post('/login',login) 
-    fastify.post('/auth',authorize)
+        registerUser,
+    );
+    fastify.post("/verify", verifyToken);
+    fastify.post("/login", login);
+    fastify.post("/auth", authorize);
 }
