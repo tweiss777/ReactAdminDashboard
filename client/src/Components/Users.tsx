acb3afa65a46909749bf0ba4ef23ad2252fd60fa
Tal Weiss <Tweiss747@gmail.com>
2024-04-14 22:30:13 +0300

    refactored user component to consume the useUserService hook

--- client/src/Components/Users.tsx
+++ client/src/Components/Users.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect, useMemo, useRef } from "react";
-import { getUsers, getUserCount, updateUser } from "../services/users.service";
+import { useState, useMemo } from "react";
 import { User as IUser } from "../types/Users";
 import User from "./User";
 import { Table, Alert, Input, Pagination } from "antd";
@@ -7,221 +6,161 @@ import { SearchOutlined } from "@ant-design/icons";
 import type { TableProps } from "antd";
 
 import { useSearchParams } from "react-router-dom";
-import { useThemeContext } from '../hooks/useThemeContext'
+import { useThemeContext } from "../hooks/useThemeContext";
+import useUserService from "../hooks/useUsersService";
 import "../scss/Users.scss";
 import "../scss/DarkMode.scss";
 export default function Users() {
-    const [users, setUsers] = useState<IUser[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [hasError, setHasError] = useState<boolean>(false);
-    const [searchParams, setSearchParams] = useSearchParams();
-    const [selectedUser, setSelectedUser] = useState<IUser | null>(null);
-    const [name, setName] = useState<string>("");
-    const [validationErrors, setValidationErrors] = useState<string[]>([]);
-    const [saveSuccesful, setSaveSuccesful] = useState<boolean>(false);
-    const totalUsers: React.MutableRefObject<number> = useRef<number>(0);
-    const { isEnabled: darkModeEnabled } = useThemeContext();
-    const currentRouteId: string | null = useMemo<string | null>(
-        () => searchParams.get("id"),
-        [searchParams],
-    );
-    function onColClick(record: IUser) {
-        const { id } = record;
-        const urlSearchParams = new URLSearchParams();
-        urlSearchParams.set("id", id.toString());
-        setSearchParams(urlSearchParams);
-        setSelectedUser(record);
-    }
-    function closeUserForm() {
-        const urlSearchParams = new URLSearchParams();
-        urlSearchParams.delete("id");
-        setSearchParams(urlSearchParams);
-        setValidationErrors([]);
-    }
-    async function onPageChange(page: number, _pageSize: number) {
-        let results;
-        if (!name) {
-            results = await getUsers(page);
-        } else {
-            results = await getUsers(page, name);
-        }
-        setUsers(results);
-    }
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [selectedUser, setSelectedUser] = useState<IUser | null>(null);
+  const { isEnabled: darkModeEnabled } = useThemeContext();
+  const {
+    users,
+    totalUsers,
+    loadingUsers,
+    validationErrors,
+    setValidationErrors,
+    searchByName,
+    saveSuccessful,
+    saveUser,
+    hasError,
+    onPageChange,
+  } = useUserService();
+  const currentRouteId: string | null = useMemo<string | null>(
+    () => searchParams.get("id"),
+    [searchParams],
+  );
+  function onColClick(record: IUser) {
+    const { id } = record;
+    const urlSearchParams = new URLSearchParams();
+    urlSearchParams.set("id", id.toString());
+    setSearchParams(urlSearchParams);
+    setSelectedUser(record);
+  }
 
-    async function saveUser(userToUpdate: IUser) {
-        try {
-            setHasError(false);
-            setValidationErrors([]);
-            const rowsChanged: number = await updateUser(userToUpdate);
-            if (rowsChanged > 0) {
-                setSaveSuccesful(true);
-                const updatedIndex = users.findIndex(
-                    (user) => user.id === userToUpdate.id,
-                );
-                const updatedUsers = [...users];
-                updatedUsers[updatedIndex] = { ...userToUpdate };
-                setUsers(updatedUsers);
-            }
-        } catch (error: any) {
-            switch (error.response.status) {
-                case 400:
-                    setValidationErrors(error.response.data.data.errors);
-                    break;
-                default:
-                    setHasError(true);
-                    break;
-            }
-        }
-    }
+  function closeUserForm() {
+    const urlSearchParams = new URLSearchParams();
+    urlSearchParams.delete("id");
+    setSearchParams(urlSearchParams);
+    setValidationErrors([]);
+  }
 
-    async function searchByName(name: string) {
-        let results: IUser[];
-        if (!name) {
-            results = await getUsers();
-            totalUsers.current = await getUserCount();
-        } else {
-            results = await getUsers(undefined, name);
-            totalUsers.current = await getUserCount(name);
-        }
-        setUsers(results);
-        setName(name);
-    }
-
-    const columns: TableProps<IUser>["columns"] = [
-        {
-            title: "id",
-            dataIndex: "id",
-            key: "id",
-            onCell: (record, _row) => {
-                return {
-                    onClick: () => onColClick(record),
-                };
-            },
-        },
-
-        {
-            title: "First Name",
-            dataIndex: "firstName",
-            key: "firstName",
-
-            onCell: (record, _row) => {
-                return {
-                    onClick: () => onColClick(record),
-                };
-            },
-        },
-        {
-            title: "Last Name",
-            dataIndex: "lastName",
-            key: "lastName",
-            onCell: (record, _row) => {
-                return {
-                    onClick: () => onColClick(record),
-                };
-            },
-        },
-        {
-            title: "Email",
-            dataIndex: "email",
-            key: "email",
-            onCell: (record, _row) => {
-                return {
-                    onClick: () => onColClick(record),
-                };
-            },
-        },
-
-        {
-            title: "Address",
-            dataIndex: "address",
-            key: "address",
-            onCell: (record, _row) => {
-                return {
-                    onClick: () => onColClick(record),
-                };
-            },
-        },
-
-        {
-            title: "IP Address",
-            dataIndex: "ipAddress",
-            key: "ipAddress",
-            onCell: (record, _row) => {
-                return {
-                    onClick: () => onColClick(record),
-                };
-            },
-        },
-    ];
-
-    useEffect(() => {
-         const fetchUsers = async () => {
-            try {
-                const [users, totalCount]: [IUser[], number] = await Promise.all([
-                    getUsers(),
-                    getUserCount(),
-                ]);
-                setUsers(users);
-                totalUsers.current = totalCount;
-            } catch (error: any) {
-                setHasError(true);
-            } finally {
-                setIsLoading(false);
-            }
+  const columns: TableProps<IUser>["columns"] = [
+    {
+      title: "id",
+      dataIndex: "id",
+      key: "id",
+      onCell: (record, _row) => {
+        return {
+          onClick: () => onColClick(record),
         };
-        fetchUsers();
-    }, []);
+      },
+    },
 
-    return (
-        <>
-            {saveSuccesful && (
-                <Alert type={"success"} message={"User updated succesfully"} />
-            )}
-            {hasError ? (
-                <Alert type={"error"} message={"Error Loading Data"} />
-            ) : (
-                <div className={`users-table ${darkModeEnabled ? `dark-mode-bg` : ""}`}>
-                    <div className="header">
-                        <h1 className={`${darkModeEnabled ? "dark-mode-text" : ""}`}>
-                            Users
-                        </h1>
-                        <div className="search-bar">
-                            <Input
-                                onChange={(event) => searchByName(event.target.value)}
-                                addonBefore={<SearchOutlined />}
-                                placeholder="Search By Name"
-                            />
-                        </div>
-                    </div>
-                    <Table
-                        className={darkModeEnabled ? "table-style-dark" : ""}
-                        scroll={{ x: true }}
-                        pagination={false}
-                        size={"large"}
-                        columns={columns}
-                        loading={isLoading}
-                        dataSource={users}
-                        bordered={true}
-                    />
-                    <Pagination
-                        className={darkModeEnabled ? "pagination-style-dark" : ""}
-                        total={totalUsers.current}
-                        pageSize={10}
-                        onChange={onPageChange}
-                    />
+    {
+      title: "First Name",
+      dataIndex: "firstName",
+      key: "firstName",
 
-                    {currentRouteId && selectedUser && (
-                        <div className="users-container">
-                            <User
-                                errors={validationErrors}
-                                onUpdate={saveUser}
-                                onClose={closeUserForm}
-                                user={selectedUser}
-                            />
-                        </div>
-                    )}
-                </div>
-            )}
-        </>
-    );
+      onCell: (record, _row) => {
+        return {
+          onClick: () => onColClick(record),
+        };
+      },
+    },
+    {
+      title: "Last Name",
+      dataIndex: "lastName",
+      key: "lastName",
+      onCell: (record, _row) => {
+        return {
+          onClick: () => onColClick(record),
+        };
+      },
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      onCell: (record, _row) => {
+        return {
+          onClick: () => onColClick(record),
+        };
+      },
+    },
+
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+      onCell: (record, _row) => {
+        return {
+          onClick: () => onColClick(record),
+        };
+      },
+    },
+
+    {
+      title: "IP Address",
+      dataIndex: "ipAddress",
+      key: "ipAddress",
+      onCell: (record, _row) => {
+        return {
+          onClick: () => onColClick(record),
+        };
+      },
+    },
+  ];
+  return (
+    <>
+      {saveSuccessful && (
+        <Alert type={"success"} message={"User updated succesfully"} />
+      )}
+      {hasError ? (
+        <Alert type={"error"} message={"Error Loading Data"} />
+      ) : (
+        <div className={`users-table ${darkModeEnabled ? `dark-mode-bg` : ""}`}>
+          <div className="header">
+            <h1 className={`${darkModeEnabled ? "dark-mode-text" : ""}`}>
+              Users
+            </h1>
+            <div className="search-bar">
+              <Input
+                onChange={(event) => searchByName(event.target.value)}
+                addonBefore={<SearchOutlined />}
+                placeholder="Search By Name"
+              />
+            </div>
+          </div>
+          <Table
+            className={darkModeEnabled ? "table-style-dark" : ""}
+            scroll={{ x: true }}
+            pagination={false}
+            size={"large"}
+            columns={columns}
+            loading={loadingUsers}
+            dataSource={users}
+            bordered={true}
+          />
+          <Pagination
+            className={darkModeEnabled ? "pagination-style-dark" : ""}
+            total={totalUsers}
+            pageSize={10}
+            onChange={onPageChange}
+          />
+
+          {currentRouteId && selectedUser && (
+            <div className="users-container">
+              <User
+                errors={validationErrors}
+                onUpdate={saveUser}
+                onClose={closeUserForm}
+                user={selectedUser}
+              />
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
 }
